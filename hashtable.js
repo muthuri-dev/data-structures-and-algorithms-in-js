423e55f31bf58fd2f1ec137c25f9a2a0dc0b95b9
muthuri-dev <muthurikennedy082@gmail.com>
2024-04-01 10:06:18 +0300

    faet(HashTable): Create hash table which gets user input and performs looksup in the table

---
+++ hashtable.js
@@ -0,0 +1,59 @@
+// HashTable class aand you can change the size of the table;
+class HashTable {
+  constructor(size = 10) {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  // Hash function
+  hash(key) {
+    let hash = 0;
+
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i)) % this.size; //example "muthuri".charCodeAt(0)= 109; then we find the modulas (the size of the hashtable) like 109%11 or 109%12 0r 109%6 etc  so 109%10 = 9 which will fit inside;
+    }
+    return hash;
+  }
+
+  // Insert key-value pair into the hash table
+  insert(key, value) {
+    const index = this.hash(key);
+    if (!this.table[index]) {
+      this.table[index] = [];
+    }
+    this.table[index].push({ key, value });
+  }
+
+  // Check if a key exists in the hash table
+  search(key) {
+    const index = this.hash(key);
+    if (!this.table[index]) return false;
+    //looping because there could be a collisiong resulting to linkedList
+    for (let item of this.table[index]) {
+      if (item.key === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+// Example usage
+const hashTable = new HashTable();
+
+//getting the data from the file
+const fs = require("fs");
+const words = fs.readFileSync("./data/data.txt", "utf8").split("\n");
+
+//feeding words to the hash table
+words.forEach((word) => hashTable.insert(word.trim(), true));
+
+// getting user input
+const userInput = prompt(
+  "Guess name which exists ☺️ and I will tell you if it exists ☺️"
+);
+
+// Checking if userinput exists in the hash table
+const exists = hashTable.search(userInput);
+
+console.log(exists);
